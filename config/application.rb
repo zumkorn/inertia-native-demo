1b3384fbf6ac1c75f642a7f4d73bc221ac30c750
Sergey Korolev <skorolev@sent.com>
2026-06-23 16:59:28 +0300

    Set up production deployment with Kamal

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module HotwireNativeDemo
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_storage.variant_processor = :disabled
   end
 end
--- config/initializers/inertia_rails.rb
+++ config/initializers/inertia_rails.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 InertiaRails.configure do |config|
-  config.version = ViteRuby.digest
+  config.version = RailsVite.manifest.digest
   # History encryption needs Web Crypto (crypto.subtle), which is only available
   # in a secure context (HTTPS). Hotwire Native loads the dev server over plain
   # http://<lan-ip>, so encryption would throw "Unable to encrypt history".
